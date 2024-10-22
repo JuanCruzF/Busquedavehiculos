9ea16df91cbea1cf6539b943656c4db6d8f13b7e
JuanCruzF <juancruzfucile@hotmail.com>
2024-10-21 23:10:35 -0300

    First Commit

---
+++ js/db.js
@@ -0,0 +1,174 @@
+const autos = [
+	{
+		marca: 'Toyota',
+		modelo: 'Corolla',
+		year: 2024,
+		precio: 30000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Honda', 
+        modelo: 'Civic', 
+        year: 2024, 
+        precio: 40000, 
+        puertas: 4, 
+        color: 'Negro', 
+        transmision: 'automatico' 
+    },
+	{
+		marca: 'Nissan',
+		modelo: 'Altima',
+		year: 2018,
+		precio: 20000,
+		puertas: 2,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Mazda', 
+        modelo: 'Mazda6', 
+        year: 2024, 
+        precio: 35000, 
+        puertas: 4, 
+        color: 'Negro', 
+        transmision: 'automatico' 
+    },
+	{
+		marca: 'Subaru',
+		modelo: 'Outback',
+		year: 2020,
+		precio: 70000,
+		puertas: 4,
+		color: 'Rojo',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Lexus',
+		modelo: 'ES 350',
+		year: 2019,
+		precio: 25000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Hyundai',
+		modelo: 'Elantra',
+		year: 2022,
+		precio: 60000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
+	{ 
+        marca: 'Kia', 
+        modelo: 'Stinger', 
+        year: 2023, 
+        precio: 80000, 
+        puertas: 2, 
+        color: 'Rojo', 
+        transmision: 'manual' 
+    },
+	{
+		marca: 'Tesla',
+		modelo: 'Model S',
+		year: 2022,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Jaguar', 
+        modelo: 'XF', 
+        year: 2021, 
+        precio: 55000, 
+        puertas: 2, 
+        color: 'Negro', 
+        transmision: 
+        'manual' 
+    },
+	{
+		marca: 'Porsche',
+		modelo: '911',
+		year: 2024,
+		precio: 25000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
+	{
+		marca: 'Lexus',
+		modelo: 'RX 350',
+		year: 2018,
+		precio: 45000,
+		puertas: 4,
+		color: 'Azul',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Toyota',
+		modelo: 'Camry',
+		year: 2019,
+		precio: 90000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Nissan', 
+        modelo: 'GT-R', 
+        year: 2017, 
+        precio: 60000, 
+        puertas: 2, 
+        color: 'Negro', 
+        transmision: 'manual' 
+    },
+	{
+		marca: 'Chevrolet',
+		modelo: 'Malibu',
+		year: 2015,
+		precio: 35000,
+		puertas: 2,
+		color: 'Azul',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Volkswagen',
+		modelo: 'Passat',
+		year: 2018,
+		precio: 50000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Volvo',
+		modelo: 'S60',
+		year: 2017,
+		precio: 80000,
+		puertas: 4,
+		color: 'Negro',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Genesis',
+		modelo: 'G70',
+		year: 2018,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Alfa Romeo', 
+        modelo: 'Giulia', 
+        year: 2016, 
+        precio: 30000, 
+        puertas: 4, 
+        color: 'Azul', 
+        transmision: 'automatico' 
+    }
+];
